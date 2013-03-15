80171d992da1fdd764e3bd6cad1614defbb63024
Sebastian <sebas.goldberg@gmail.com>
2013-03-15 08:06:12 -0700

    Primer commit. Desacople de aplicaciones cross del proyecto agencia.

---
+++ correo/mail.py
@@ -0,0 +1,36 @@
+# coding=utf-8
+from django.core.mail import EmailMultiAlternatives
+from django.conf import settings
+
+class Mail(EmailMultiAlternatives):
+
+  def actualizar_asunto(self,asunto):
+    return asunto
+
+  def get_reply_to(self):
+    return None
+
+  def __init__(self,asunto, cuerpo_de_texto, destinatarios,ccs=None):
+
+    if not settings.AMBIENTE.email.user:
+      raise Exception(u'No se ha definido settings.AMBIENTE.email.user')
+    
+    _asunto = self.actualizar_asunto(asunto)
+    reply_to=self.get_reply_to()
+    if reply_to:
+      _headers = {'Reply-To': reply_to}
+    
+    self.mensaje = EmailMultiAlternatives(
+      _asunto,
+      cuerpo_de_texto,
+      settings.AMBIENTE.email.user,
+      destinatarios,
+      headers = _headers,
+      cc=ccs
+    )
+
+  def set_html_body(self,html_content):
+    self.mensaje.attach_alternative(html_content, "text/html")
+
+  def send(self):
+    self.mensaje.send()
---
+++ telefono/admin.py
@@ -0,0 +1,4 @@
+from django.contrib import admin
+from iamsoft.cross.telefono.models import Compania
+
+admin.site.register(Compania)
