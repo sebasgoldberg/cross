5b940b4c729fe14cf17b1a5fb3ec7eac6c0e3576
Sebastian Goldberg <sebas.goldberg@gmail.com>
2014-03-31 15:48:39 -0700

    Fix error de validación en hora de disponibilidad.

--- disponibilidad/models.py
+++ disponibilidad/models.py
@@ -21,12 +21,13 @@ class Disponibilidad(models.Model):
 
   dia_desde = models.IntegerField(max_length=2,choices=DIAS_SEMANA, verbose_name=ugettext_lazy(u'Día desde'))
   dia_hasta = models.IntegerField(max_length=2,choices=DIAS_SEMANA, verbose_name=ugettext_lazy(u'Día hasta'), null=False, blank=False)
-  hora_desde = models.TimeField(verbose_name=ugettext_lazy(u'Hora desde'))
-  hora_hasta= models.TimeField(verbose_name=ugettext_lazy(u'Hora hasta'))
+  hora_desde = models.TimeField(verbose_name=ugettext_lazy(u'Hora desde'), null=True, blank=True)
+  hora_hasta= models.TimeField(verbose_name=ugettext_lazy(u'Hora hasta'), null=True, blank=True)
 
   def clean(self):
-    if self.hora_desde >= self.hora_hasta:
-      raise ValidationError(_(u'La hora desde debe ser menor a la hora hasta.'))
+    if self.hora_desde and self.hora_hasta:
+      if self.hora_desde >= self.hora_hasta:
+        raise ValidationError(_(u'La hora desde debe ser menor a la hora hasta.'))
 
   class Meta:
     abstract = True
