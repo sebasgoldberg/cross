cbfa17e3522c8dbb59f5325643126d4a64b2d003
Sebastian Goldberg <sebas.goldberg@gmail.com>
2014-04-06 12:35:05 -0700

    Se quita el puerto de las urls cuando coincide con el estándar del protocolo.

--- ambiente/models.py
+++ ambiente/models.py
@@ -6,7 +6,11 @@ class BaseAmbiente:
     return self.get_base_http()
 
   def get_base_http(self):
+    if int(self.puerto_http) == 80:
+      return 'http://%s' % self.dominio
     return 'http://%s:%s'%(self.dominio,self.puerto_http)
 
   def get_base_https(self):
+    if int(self.puerto_https) == 443:
+      return 'https://%s' % self.dominio
     return 'https://%s:%s'%(self.dominio,self.puerto_https)
